ec54abc52065275ecf9448ccfa2c9adc05645c77
haozch <haozch@minstone.com.cn>
2021-02-25 23:54:15 +0800

    头部更新

--- src/entry/components/howeHeader.vue
+++ src/entry/components/howeHeader.vue
@@ -2,14 +2,37 @@
   <div i="howe-header-index">
     <div class="howe-header-boxs">
       <!--      头部logo-->
-      <div class="header-logo">
-        <img src="@/assets/images/header-logo.png" alt="">
-        <span class="header-name">技术大脑</span>
+      <div class="header-logo header-left">
+        <img class="logo-item" src="@/assets/images/header-logo.png" alt="">
+        <span class="header-name logo-item">技术大脑</span>
       </div>
-      <!--      头部标签-->
-      <div class="header-tag">
+      <div class="header-right">
+        <!--      头部标签-->
+        <div class="header-tab">
+          <div class="tab-search-input">
+            <el-input placeholder="请输入搜索的内容" prefix-icon="el-icon-search" v-model="searchTip">
+            </el-input>
+          </div>
+          <div class="tab-content">
+            <ul class="tab-list">
+              <li class="tab-item" v-for="(item,index) in tabList" :key="index">{{item.tabName}}</li>
+            </ul>
+          </div>
+        </div>
+        <!-- 用户信息 -->
+        <div class="header-user">
+          <!-- 未登录，及游客展示的 -->
+          <div class="unsign-up">
+            <el-button type="primary" round>登录</el-button>
+            <el-button type="warning" round>注册</el-button>
+          </div>
+          <!-- 已登录用户展示的信息 -->
+          <div class="sign-up user-info">
 
+          </div>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -19,6 +42,24 @@
     name: "howeHeader",
     data() {
       return {
+        searchTip: "", //搜索的关键字
+        tabList: [{
+            tabName: "前端",
+            tabId: "1"
+          },
+          {
+            tabName: "后端",
+            tabId: "2"
+          },
+          {
+            tabName: "算法",
+            tabId: "3"
+          },
+          {
+            tabName: "其他",
+            tabId: "4"
+          }
+        ]
       }
     },
     components: {},
@@ -31,8 +72,7 @@
     },
 
     /* 模板编译/挂载之后 */
-    mounted() {
-    },
+    mounted() {},
 
     /* 组件销毁前 */
     beforeDestroy() {
@@ -58,24 +98,88 @@
     user-select: none;
 
     .howe-header-boxs {
-      padding: .7rem 1.5rem;
-      line-height: 2.2rem;
+
+      position: relative;
+      line-height: 2.8rem;
       transition: transform .3s;
       background: $color-white;
-      .header-logo{
-        cursor: pointer;
-        img{
-          height: 2.2rem;
-          min-width: 2.2rem;
-          margin-right: .8rem;
-          vertical-align: top;
-        }
-        .header-name{
-          font-size: 28px;
-          font-weight: 600;
-          color: $color-dominant;
-        }
+      vertical-align: top;
+      padding: 0 1rem;
+
+      .header-left,
+      .header-right {
+        display: inline-block;
+        vertical-align: middle;
       }
+        /* 头部logo样式 */
+        .header-logo {
+         
+          cursor: pointer;
+          img {
+            height: 2.2rem;
+            min-width: 2.2rem;
+            margin-right: .8rem;
+            vertical-align: top;
+          }
+
+          .header-name {
+            font-size: 28px;
+            font-weight: 600;
+            color: $color-dominant;
+          }
+        }
+
+
+      .header-right {
+        position: absolute;
+        right: 0px;
+
+        .header-tab,
+        .header-user {
+          display: inline-block;
+        }
+
+        /* 搜索框和tab样式 */
+        .header-tab {
+          padding: 0 20px;
+
+          .tab-search-input,
+          .tab-content {
+            display: inline-block;
+            vertical-align: top;
+          }
+
+          .tab-search-input {
+            .el-input .el-input__inner {
+              height: 32px;
+              border-radius: 20px;
+            }
+
+          }
+
+          .tab-content {
+            font-size: 18px;
+
+            .tab-list {
+              .tab-item {
+                float: left;
+                margin-left: 20px;
+
+                &:hover {
+                  color: $color-sidebar-hover;
+                  background: $color-background-3;
+                }
+              }
+            }
+
+          }
+        }
+
+        /* 用户和游客样式 */
+        .header-user {}
+      }
+
+
     }
   }
 </style>
